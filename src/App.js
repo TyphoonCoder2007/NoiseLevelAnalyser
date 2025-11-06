ddccbb0ea137445c9b3f37d23ddb7230e262ca3b
NIRMALYA GHOSH <nirmalyaghosh2127@gmail.com>
2025-11-06 18:04:55 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import NoiseTracker from './components/NoiseTracker';
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <NoiseTracker />
+    </div>
+  );
+}
+
+export default App;
